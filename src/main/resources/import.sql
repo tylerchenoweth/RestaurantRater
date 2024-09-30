88f05d5aa327e680fb96b9383e39a1d3cfd354d6
sh4ck1et0n <tchen358@gmail.com>
2024-09-30 00:54:09 -0400

    used to populate database on boot

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO restaurant (name, city, zipcode) VALUES ('Sample Name', 'Sample City', '12345');
+INSERT INTO restaurant (id, name, city, ZIPCODE) VALUES (1, 'Pizza Planet', 'Tampa', 12345);
+INSERT INTO restaurant (id, name, city, ZIPCODE) VALUES (2, 'Dorsia', 'New York City', 54321);
+INSERT INTO restaurant (id, name, city, ZIPCODE) VALUES (3, 'Los Pollos Hermanos', 'Albaquerque', 13579);
+INSERT INTO restaurant (id, name, city, ZIPCODE) VALUES (4, 'The Three Broomsticks', 'Hogsmeade', 24680);
+INSERT INTO restaurant (id, name, city, ZIPCODE) VALUES (5, 'Mos Eisley Cantina', 'Tatooine', 09876);
+
+
